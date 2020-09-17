205c8dcdbcd77dd3f7872ca74b2327bcba56db26
jarus <surajkumarithms@gmail.com>
2020-09-17 11:18:54 +0500

    complete  backend

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+const connectDB = require("./config/db")
 const app = express();
 const Port = process.env.PORT || 5000;
+connectDB();
 
+app.use(express.json({extended :false}));
 app.use("/api/users",require("./routes/users"))
 app.use("/api/contacts",require("./routes/contacts"))
 app.use("/api/auth",require("./routes/auth"))
