1d8c8e7962515e6b5a7e8a7d66810fff6be6de45
jarus <surajkumarithms@gmail.com>
2020-09-19 16:29:14 +0500

    perparing for deply

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db")
 const app = express();
+const path = require("path")
 const Port = process.env.PORT || 5000;
 connectDB();
 
@@ -9,7 +10,12 @@ app.use("/api/users",require("./routes/users"))
 app.use("/api/contacts",require("./routes/contacts"))
 app.use("/api/auth",require("./routes/auth"))
 
-
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+    })
+}
 app.listen(Port,()=>{
     console.log(`Server is up on port ${Port}`);
 })
