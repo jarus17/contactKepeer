bcd7483ce7464928c7e72edf755f6e1e1c468d33
jarus <surajkumarithms@gmail.com>
2020-09-21 10:13:51 +0500

    finnal v

--- client/src/context/contact/ContactReducer.js
+++ client/src/context/contact/ContactReducer.js
@@ -41,7 +41,8 @@ export default (state,action)=>{
         case UPDATE_CONTACT:
             return {
                 ...state,
-                contacts :state.contacts.map(contact => contact.id === action.payload.id?action.payload:contact )
+                contacts :state.contacts.map(contact => contact.id === action.payload.id?action.payload:contact ),
+                loading:false
             }
         case FILTER_CONTACTS:
             return {
